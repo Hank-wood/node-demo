d4143a2b8143df41a9472f1bed0e4ba79d0bc8a5
古德God <13823248710@163.com>
2016-05-04 09:05:20 +0800

    注册密码加盐

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,10 @@
 var mongoose = require('../config/connectMongo.js');
-
+var bcrypt = require('bcrypt');
 var userSchema = new mongoose.Schema({
-    username: String,
+    username: {
+    	unique: true,
+    	type: String
+    },
     password: String
 })
 var User = mongoose.model('user',userSchema,'user');
@@ -14,10 +17,8 @@ exports.index = function(req, res, next) {
 /*注册*/
 exports.register = function(req, res, next) {
     var reg = /^\s*$/;
-    var user = new User({
-        username: req.body.username,
-        password: req.body.password
-    })
+    var SALT_WORK_FACTOR = 10;
+    
     if(!req.body || reg.test(req.body.username)){
         res.send({
             code: 0,
@@ -32,16 +33,36 @@ exports.register = function(req, res, next) {
         })
         return false;
     }
-    user.save(function(err){
-        if(err){
-            res.send({
-                code: 0,
-                msg: err
-            })
-        }
-        res.send({
-            code: 1,
-            msg: 'success'
-        })
+    bcrypt.genSalt(SALT_WORK_FACTOR,function(err,salt){
+    	if(err)return next(err);
+    	bcrypt.hash(req.body.password, salt, function(err, hash){
+    		if(err)return next(err);
+    		req.body.password = hash;
+    		var user = new User({
+    		    username: req.body.username,
+    		    password: req.body.password
+    		})
+    		User.find({username: req.body.username},function(err, user){
+    			if(err)return console.error(err);
+    			if(user){
+    				return console.error('username 重复')
+    			}else{
+
+    			}
+    		})
+			user.save(function(err){
+			    if(err){
+    		        res.send({
+    		            code: 0,
+    		            msg: err
+    		        })
+    		    }
+    		    res.send({
+    		        code: 1,
+    		        msg: 'success'
+    		    })
+    		})
+    	})
     })
+    
 };
