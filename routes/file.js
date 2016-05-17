420ef0d01294ab91b8e68a33e1a676430624b0ad
王美建 <wangmeijian@360.cn>
2016-05-17 19:19:20 +0800

    文件上传没有扩展名？

--- routes/file.js
+++ routes/file.js
@@ -1,6 +1,9 @@
 exports.upload = function(req, res, next) {
+    console.log(req.files)
     if(req.files){
-        next();
+        res.send({
+            code: 1
+        })
     }else{
         res.send({
             code: 0
